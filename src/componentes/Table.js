2c4a9a2cd87709afbc55f009ac1bac13e153f165
Samuel Guilherme <samuel.guilherme.l@hotmail.com>
2021-09-16 19:34:48 -0300

    requisito 3 e lint

--- src/componentes/Table.js
+++ src/componentes/Table.js
@@ -20,9 +20,21 @@ function Table() {
 
   const [filtered, setfiltered] = useState([]);
 
+  const [SetColumns] = useState(
+    ['population', 'orbital_period', 'rotation_period', 'surface_water', 'diameter'],
+  );
+  // Requisito 03
+  const [filterByNumericValues, setfilterByNumericValues] = useState(
+    {
+      column: 'population',
+      comparison: '',
+      value: '100000',
+    },
+  );
+
   useEffect(() => {
     function filterByInput() {
-      let newArray = data;
+      let newArray = [...data];
       if (newArray.length > 0 && filter.filterByName.name) {
         newArray = newArray
           .filter((item) => item.name.toLowerCase()
@@ -34,6 +46,20 @@ function Table() {
   },
   [data, filter]);
 
+  const handleFilterByNumericValue = () => {
+    const { column, comparison, value } = filterByNumericValues;
+    switch (comparison) {
+    case 'maior que':
+      return setfiltered(filtered.filter((planet) => planet[column] > Number(value)));
+    case 'menor que':
+      return setfiltered(filtered.filter((planet) => planet[column] < Number(value)));
+    case 'igual a':
+      return setfiltered(filtered.filter((planet) => planet[column] === value));
+    default:
+      return filtered;
+    }
+  };
+
   return (
     <>
       <input
@@ -41,6 +67,58 @@ function Table() {
         onChange={ (e) => handleChange(e) }
         data-testid="name-filter"
       />
+      <form>
+        <label htmlFor="column">
+          <select
+            name="column"
+            id="column"
+            data-testid="column-filter"
+            onChange={ ({ target }) => (setfilterByNumericValues({
+              ...filterByNumericValues,
+              column: target.value,
+            })) }
+          >
+            {SetColumns.map((column, index) => (
+              <option key={ index } value={ column }>{ column }</option>)) }
+          </select>
+        </label>
+        <label htmlFor="comparison">
+          <select
+            data-testid="comparison-filter"
+            id="comparison"
+            name="comparison"
+            onChange={ ({ target }) => (setfilterByNumericValues({
+              ...filterByNumericValues,
+              comparison: target.value,
+            })) }
+          >
+            <option value="" defaultValue>Selecione sua opção</option>
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </label>
+        <label htmlFor="value">
+          <input
+            type="number"
+            data-testid="value-filter"
+            id="value"
+            name="value"
+            onChange={ ({ target }) => (setfilterByNumericValues({
+              ...filterByNumericValues,
+              value: target.value,
+            })) }
+          />
+        </label>
+        <button
+          data-testid="button-filter"
+          type="button"
+          name="add-filter"
+          onClick={ handleFilterByNumericValue }
+        >
+          Filter
+        </button>
+      </form>
       <table>
         <tbody>
           <tr>
