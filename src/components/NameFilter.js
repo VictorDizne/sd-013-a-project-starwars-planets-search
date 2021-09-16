05b3f055da002b18b44d7461cbb5973a21e4c79f
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-09-16 18:32:17 -0300

    Corrige lint do segundo requisito

--- src/components/NameFilter.js
+++ src/components/NameFilter.js
@@ -2,19 +2,7 @@ import React, { useContext } from 'react';
 import Context from './mycontext';
 
 const NameFilter = () => {
-  const { filters: { filterByName }, data, setPlanets } = useContext(Context);
-
-  // function changePlanetsArray(newPlanets) {
-  //   if (newPlanets !== data) {
-  //     console.log(newPlanets);
-  //     setPlanets(newPlanets);
-  //   }
-  // }
-
-  // if (filterByName.name !== '') {
-  //   const newPlanets = data.filter((planet) => planet.name.includes(filterByName.name));
-  //   changePlanetsArray(newPlanets);
-  // }
+  const { filters: { filterByName } } = useContext(Context);
 
   return (
     <label htmlFor="nameFilter">
