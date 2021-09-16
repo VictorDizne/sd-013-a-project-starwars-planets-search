e6d56fea89f409e4e23d1fbf2bfb4ff852a7c9d0
Icaro Ferreira <icaro.iky@gmail.com>
2021-09-16 19:36:06 -0300

    Req 1 corrigido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Table from './components/Table';
+import Provider from './context/Provider';
 
 function App() {
   return (
     <div>
-      <Table />
+      <Provider>
+        <Table />
+      </Provider>
     </div>
   );
 }
