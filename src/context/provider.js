01f9cac5d61e0664bcc95abe1a794df6aafbaacf
ALINE <hoshinoaline@yahoo.com.br>
2021-09-17 00:08:07 -0300

    proptypes

--- src/context/provider.js
+++ src/context/provider.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import StarsContext from './StarContext';
 
 function Provider({ children }) {
@@ -22,4 +23,14 @@ function Provider({ children }) {
   );
 }
 
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 export default Provider;
+
+// fonte propTypes do children https://spectrum.chat/react/general/children-is-missing-in-props-validation~0b004d6e-4f9e-44e5-a039-21f23e7c7c5d
+// explicação do node:  // Anything that can be rendered: numbers, strings, elements or an array
+// (or fragment) containing these types.
+// optionalNode: PropTypes.node,
+// fonte: https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html
