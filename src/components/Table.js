8d1385f2a24891eacebf6239329285e1a37591f3
Diego Vilela <vilelaphp@gmail.com>
2021-09-18 22:30:05 -0300

    Add a <Table />

---
+++ src/components/Table.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { Context } from '../context/PlanetProvider';
+import TableRow from './TableRow';
+
+function Table() {
+  const { planets, isFetching } = useContext(Context);
+
+  if (isFetching) return <h1>Loading...</h1>;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        {planets.map((planet) => <TableRow key={ planet.name } planet={ planet } />)}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
