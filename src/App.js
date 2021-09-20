4a11d5dec7d49719aae58c1515a0018293590cf1
Ramon Vitor <vitorramon@hotmail.com>
2021-09-20 16:11:26 -0300

    Adicionado como comentario no arquivo APP as pessoas que me ajudaram

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import StarWarsPlanetsProvider from './context/StarWarsPlanetsProvider';
 import Table from './components/Table';
 
+// Projeto feito com a ajuda de Felipe Neves e Douglas Drozda
+
 function App() {
   return (
     <StarWarsPlanetsProvider>
