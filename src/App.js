4583e4996a06147ea4871e85b729d406f4baf28e
rjanovicci <rjanovicci@hotmail.com>
2021-09-20 17:13:49 -0400

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Table from './components/Table';
+import StarWarsContext from './contexts/StarWarsContext';
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const request = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const returnFetch = await request.json();
+      setData(returnFetch.results);
+    };
+    fetchAPI();
+  }, []);
+
   return (
-    <span>Hello, App!</span>
+    <StarWarsContext.Provider value={ data }>
+      <Table />
+    </StarWarsContext.Provider>
   );
 }
-
-export default App;
