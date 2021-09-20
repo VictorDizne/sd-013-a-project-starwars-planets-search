be196630a35085cb8e46df0549d9fef8f703f4ae
Diego Vilela <vilelaphp@gmail.com>
2021-09-20 14:10:00 -0300

    Improve PlanetProvider's logic

--- src/context/PlanetProvider.js
+++ src/context/PlanetProvider.js
@@ -4,16 +4,36 @@ import fetchPlanets from '../services/fetchPlanets';
 
 const Context = createContext();
 
+const initialColumns = [
+  'population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water',
+];
+
 const initialFiltersState = {
   filterByName: { name: '' },
   filterByNumericValues: [],
+};
+
+const getOperator = (filter) => {
+  const { column, comparison, value } = filter;
+
+  const gt = (planet) => Number(planet[column]) > Number(value);
+  const lt = (planet) => Number(planet[column]) < Number(value);
+  const eq = (planet) => Number(planet[column]) === Number(value);
+  if (comparison === 'maior que') return gt;
+  if (comparison === 'menor que') return lt;
+  return eq;
 };
 
 function PlanetProvider({ children }) {
   const [data, setData] = useState([]);
   const [isFetching, setIsFetching] = useState(true);
   const [filters, setFilters] = useState(initialFiltersState);
-  const [filteredPlanets, filterPlanets] = useState([]);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
+  const [columns, setColumns] = useState(initialColumns);
 
   useEffect(() => {
     const getPlanets = async () => {
@@ -26,32 +46,29 @@ function PlanetProvider({ children }) {
   useEffect(() => {
     const userInput = filters.filterByName.name;
     const result = data.filter((planet) => planet.name.includes(userInput));
-    filterPlanets(result);
+    setFilteredPlanets(result);
   }, [filters.filterByName.name, data]);
 
-  const getOperator = (filter) => {
-    const { column, comparison, value } = filter;
-
-    const gt = (planet) => Number(planet[column]) > Number(value);
-    const lt = (planet) => Number(planet[column]) < Number(value);
-    const eq = (planet) => Number(planet[column]) === Number(value);
-    if (comparison === 'maior que') return gt;
-    if (comparison === 'menor que') return lt;
-    return eq;
-  };
-
   const addFilter = (filter) => {
-    const newFilteredPlanets = filteredPlanets.filter(getOperator(filter));
-    filterPlanets(newFilteredPlanets);
+    if (!filter.column || !filter.value) return null;
 
-    setFilters({
-      ...filters,
+    setFilters({ ...filters,
       filterByNumericValues: [
         ...filters.filterByNumericValues,
         filter,
-      ],
-    });
+      ] });
+
+    // Update planets
+    const newFilteredPlanets = filteredPlanets.filter(getOperator(filter));
+    setFilteredPlanets(newFilteredPlanets);
   };
+
+  // Remove `column` already used
+  useEffect(() => {
+    const usedColumns = filters.filterByNumericValues.map((filter) => filter.column);
+    const newColumnsState = initialColumns.filter((col) => !usedColumns.includes(col));
+    setColumns(newColumnsState);
+  }, [filters.filterByNumericValues]);
 
   const context = {
     planets: filteredPlanets,
@@ -59,6 +76,7 @@ function PlanetProvider({ children }) {
     filters,
     setFilters,
     addFilter,
+    columns,
   };
 
   return (
