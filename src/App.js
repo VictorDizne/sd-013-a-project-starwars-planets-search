a6a975bc115f98357b85f4abf836fdcb8260e330
icaroMelo <exemployt@gmail.com>
2021-09-20 15:50:45 -0300

    Save changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import Context from './context/Context';
+
+https://github.com/tryber/sd-013-a-project-starwars-planets-search/pull/13/commits/a9447dc7e7c42cd6ef5fbfd7e0ba386e9cab4163
 
 function App() {
+
+  const [planets, setPlanets] = useState([1]);
+
+  const fetchApi = async () => {
+    const planetsURL = 'https://swapi-trybe.herokuapp.com/api/planets/';
+    const response = await fetch(planetsURL)
+      .then((result) => result.json());
+    setPlanets(response.results);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
-    <span>Primeiro Commit</span>
+    <Context.Provider>
+      <span>Primeiro Commit</span>
+    </Context.Provider>
   );
 }
 
