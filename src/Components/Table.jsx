518fa39f97608e70f163ded1bbb7cc582dae4f26
Adriana Barreto <adri92barreto123@gmail.com>
2021-09-23 20:26:12 -0300

    Executa primeira questao

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,11 +1,14 @@
 // import React from 'react';
 import React, { useContext } from 'react';
 import MyContext from '../Contexto/MyContext';
+import Tr from './Tr';
 
 function Table() {
   const { data } = useContext(MyContext);
 
   const header = data.length > 0 ? Object.keys(data[0]) : [];
+
+  const linhas = data.length > 0 ? data : [];
 
   return (
     <div>
@@ -13,6 +16,9 @@ function Table() {
         <thead>
           <tr>{ header.map((title) => <th key={ title }>{ title }</th>)}</tr>
         </thead>
+        <tbody>
+          { linhas.map((allData) => <Tr key={ allData.name } allData={ allData } />)}
+        </tbody>
       </table>
     </div>
   );
