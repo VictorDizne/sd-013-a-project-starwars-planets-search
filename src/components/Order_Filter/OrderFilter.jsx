70a5f210e863dc3c395b825a62361befc159c5ca
junglejf <jfcchagas@id.uff.br>
2021-09-26 22:20:59 -0300

    Requisito 6: Dropdown com o nome das colunas adicionado ao elemento OrderFilter

--- src/components/Order_Filter/OrderFilter.jsx
+++ src/components/Order_Filter/OrderFilter.jsx
@@ -1,17 +1,24 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 
-const OrderFilter = () => (
-  <div>
-    <select
-      data-testid="comparison-filter"
-      name="comparison"
-      onChange={ setComparisonOnChange }
-      value={ comparisonOption }
-    >
-      {COMPARISON_OPTIONS.map((name) => (
-        <option key={ htmlID({ name }) } value={ name }>{ name }</option>)) }
-    </select>
-  </div>
-);
+const OrderFilter = () => {
+  const { planets } = useContext(PlanetContext);
+
+  const COLUMN_NAMES = Object.keys({ ...planets[0] })
+    .map((column) => column !== 'residents'); // Pelo requisito  a coluna residents não deve ser exibida
+
+  const [columnOption, setColumnOption] = useState[(COLUMN_NAMES[0])];
+  return (
+    <div>
+      <select
+        data-testid="column-sort"
+        name="columm"
+        onChange={ ({ target: { value } }) => setColumnOption(value) }
+        value={ columnOption }
+      >
+        {COLUMN_NAMES.map((name) => (
+          <option key={ htmlID({ name }) } value={ name }>{ name }</option>)) }
+      </select>
+    </div>);
+};
 
 export default OrderFilter;
