a46dea97795d66170a204b8d83a21deb83b11629
Breno Lopes <brenokos@gmail.com>
2021-10-02 17:36:32 -0300

    add requisito 5

--- src/components/filters.js
+++ src/components/filters.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import apiContext from '../contexts/apiContext';
 
 function Filters() {
-  const { setFilters, filters } = useContext(apiContext);
+  const { setFilters, filters, setAtt, setFiltering } = useContext(apiContext);
   const [filtersInUse, setFiltersInUse] = useState([]);
   const [arrOptionFilter, setArrOptionFilter] = useState(
     ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'],
@@ -30,50 +30,79 @@ function Filters() {
     arrOptionFilter
       .splice(arrOptionFilter.indexOf(filterByNumbers.column), 1);
     setArrOptionFilter(arrOptionFilter);
-    console.log(arrOptionFilter, 'options');
+    setAtt(true);
+  };
+
+  const removeFilter = ({ target }) => {
+    setArrOptionFilter([...arrOptionFilter, target.value]);
+
+    filtersInUse.splice(filtersInUse.indexOf(target.value), 1);
+    setFiltersInUse(filtersInUse);
+
+    const filtered = filters.filterByNumericValues.filter(({ column }) => column !== target.value);
+    const filterByNumericValues = [...filtered];
+    setFilters({ ...filters, filterByNumericValues });
+    setAtt(true);
+    setFiltering(true);
   };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="busque um planeta"
-        data-testid="name-filter"
-        onChange={ handleChangeSearch }
-      />
-      <select
-        data-testid="column-filter"
-        onChange={ ({ target }) => setFilterByNumbers({
-          ...filterByNumbers,
-          column: target.value }) }
-      >
-        {arrOptionFilter
-          .map((item, i) => <option key={ i } value={ item }>{item}</option>)}
-      </select>
-      <select
-        data-testid="comparison-filter"
-        onChange={ ({ target }) => setFilterByNumbers({
-          ...filterByNumbers,
-          comparison: target.value }) }
-      >
-        <option value="maior que">maior que</option>
-        <option value="menor que">menor que</option>
-        <option value="igual a">igual a</option>
-      </select>
-      <input
-        type="number"
-        data-testid="value-filter"
-        onChange={ ({ target }) => setFilterByNumbers({
-          ...filterByNumbers,
-          value: target.value }) }
-      />
-      <button
-        type="button"
-        data-testid="button-filter"
-        onClick={ handleChangeFilter }
-      >
-        filtrar
-      </button>
+      <div>
+        <input
+          type="text"
+          placeholder="busque um planeta"
+          data-testid="name-filter"
+          onChange={ handleChangeSearch }
+        />
+        <select
+          data-testid="column-filter"
+          onChange={ ({ target }) => setFilterByNumbers({
+            ...filterByNumbers,
+            column: target.value }) }
+        >
+          {arrOptionFilter
+            .map((item, i) => <option key={ i } value={ item }>{item}</option>)}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          onChange={ ({ target }) => setFilterByNumbers({
+            ...filterByNumbers,
+            comparison: target.value }) }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          data-testid="value-filter"
+          onChange={ ({ target }) => setFilterByNumbers({
+            ...filterByNumbers,
+            value: target.value }) }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ handleChangeFilter }
+        >
+          filtrar
+        </button>
+      </div>
+      <div>
+        {filtersInUse.map((item, i) => (
+          <div key={ i } data-testid="filter">
+            <p>{item}</p>
+            <button
+              type="button"
+              onClick={ removeFilter }
+              value={ item }
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/contexts/apiProvider.js
+++ src/contexts/apiProvider.js
@@ -5,7 +5,9 @@ import apiContext from './apiContext';
 const ApiProvider = ({ children }) => {
   const [data, setData] = useState([]);
   const [loaded, setLoaded] = useState(false);
-  const [dataFiltered, setDataFiltered] = useState(data);
+  const [dataFiltered, setDataFiltered] = useState([]);
+  const [att, setAtt] = useState(false);
+  const [filtering, setFiltering] = useState(false);
   const [filters, setFilters] = useState({
     filterByName: {
       name: '',
@@ -38,37 +40,44 @@ const ApiProvider = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    const filteredName = data.filter((i) => i.name.includes(filters.filterByName.name));
-    setDataFiltered(filteredName);
-  }, [data, filters.filterByName.name]);
+    setDataFiltered(data);
+    if (filtering) {
+      setFiltering(false);
+      return setAtt(true);
+    }
+  }, [data, filtering]);
 
   useEffect(() => {
-    filters.filterByNumericValues.map(() => {
-      const { column, comparison, value } = filters
-        .filterByNumericValues[filters.filterByNumericValues.length - 1];
-
-      const filtered = dataFiltered.filter((i) => {
-        switch (comparison) {
-        case 'maior que':
-          return Number(i[column]) > value;
-        case 'menor que':
-          return Number(i[column]) < value;
-        case 'igual a':
-          return i[column] === value;
-        default:
-          return i;
-        }
+    if (att) {
+      const arr = filtering ? data : dataFiltered;
+      filters.filterByNumericValues.map(({ column, comparison, value }) => {
+        const filtered = arr.filter((i) => {
+          switch (comparison) {
+          case 'maior que':
+            return Number(i[column]) > value;
+          case 'menor que':
+            return Number(i[column]) < value;
+          case 'igual a':
+            return i[column] === value;
+          default:
+            return i;
+          }
+        }).filter(({ name }) => name.includes(filters.filterByName.name));
+        console.log('atualizou');
+        // console.log(dataFiltered);
+        setDataFiltered(filtered);
+        return setAtt(false);
       });
-      // console.log(dataFiltered);
-      return setDataFiltered(filtered);
-    });
-  }, [filters, filters.filterByNumericValues]);
+    }
+  }, [att, dataFiltered, filters]);
 
   const states = {
     dataFiltered,
     data,
     loaded,
     setFilters,
+    setFiltering,
+    setAtt,
     filters };
 
   return (
