4e3f552029d0113052f64884c5eb6a50dafee57b
João Paulo <jp.viegas@hotmail.com>
2021-10-05 11:39:03 -0300

    teste

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Table = () => (
-  <table />
-);
+const Table = () => {
+  fetch('https://swapi-trybe.herokuapp.com/api/planets/?format=json')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+  return <table />;
+};
 
 export default Table;
