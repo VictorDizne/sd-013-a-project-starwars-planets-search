e5425cd25567dadad72441031a4c94e44480ba17
cristianovitorino <57003922+cristianovitorino@users.noreply.github.com>
2021-10-06 13:58:35 -0300

    Revisa 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import './App.css';
-import Table from './components/Table';
-import ContextProvider from './contextAPI/PlanetsProvider';
-import FilterForm from './components/FilterForm';
-import FilterView from './components/FilterView';
 
 function App() {
   return (
-    <ContextProvider>
-      <FilterForm />
-      <FilterView />
-      <Table />
-    </ContextProvider>
+    <span>Hello, App!</span>
   );
 }
 
--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-const fetchStarWarsPlanets = async () => {
-  const search = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
-  const response = await search.json();
-  const { results } = response;
-
-  return results; // Retorna o array
-};
-
-export default fetchStarWarsPlanets;
