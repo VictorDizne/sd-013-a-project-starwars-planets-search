08e6d7f1750ce27be78d24c2e265c9ae2eded1ba
Anibal <anibalneto29@gmail.com>
2021-10-11 22:54:40 -0300

    Table development

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,16 +1,43 @@
 import React, { useContext } from 'react';
-import API from '../services/API';
 import myContext from '../context/Context';
 
 const Table = () => {
   const { data } = useContext(myContext);
-  API();
-  console.log(data);
+  const columnHead = Object.keys([0]);
+  const header = columnHead.map((tagColumnHead, index) => {
+    return (
+      <th key={ index }>
+        { tagColumnHead }
+      </th>
+    );
+  });
+
+  const body = data.map((results, index) => {
+    if (results !== 'residents') {
+      const result = Object.values(results);
+      return (
+        <tr key={ index }>
+          { result.map((planetEntry) => <td key={ planetEntry }>{ planetEntry }</td>)}
+        </tr>
+      );
+    }
+    return null;
+  });
+
   return (
-    <p>
-      Parágrafo
-    </p>
+    <table>
+      <thead>
+        <tr>
+          { header }
+        </tr>
+      </thead>
+      <tbody>
+        { body }
+      </tbody>
+    </table>
   );
 };
 
 export default Table;
+
+// Source: consulta ao repositório da Elaine - turma13A
