ed71945c3ef3ce3f09d542a16a154a6c4ce1a118
FernandoLimas <nando_filipe@hotmail.com>
2021-10-20 14:20:04 -0300

    requisito 1

---
+++ src/API/getAPI.js
@@ -0,0 +1,9 @@
+// function fetchAPI() {
+//   return (
+//     fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+//       .then((obj) => obj.json())
+//       .then(({ result }) => result)
+//   );
+// }
+// console.log(fetchAPI());
+// export default fetchAPI;
---
+++ src/context/MyProvider.js
@@ -0,0 +1,32 @@
+// Requisito 1
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import MyContext from './MyContext';
+
+function MyProvider({ children }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchPlanet = () => {
+      fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+        .then((obj) => obj.json())
+        .then(({ results }) => setData(results));
+    };
+    fetchPlanet();
+  }, []);
+
+  const contextValue = {
+    data,
+  };
+  
+  return (
+    <MyContext.Provider value={ contextValue }>
+      {children}
+    </MyContext.Provider>
+  );
+}
+MyProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default MyProvider;
