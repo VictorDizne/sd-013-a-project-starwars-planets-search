d3b2cba11d441ca212a75bdaa0f170d59c4edd25
Walace Nascimento <wlacens@hotmail.com>
2021-10-21 07:31:23 -0300

    Excluindo as pastas page, context e componentes, recomeçar e preciso

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { PlanetsProvider } from './context/PlanetsContext';
-import Table from './pages/Table';
 
 function App() {
   return (
-    <PlanetsProvider>
-      <nav>
-        <span>Hello, Jedi Universe!</span>
-      </nav>
-      <main>
-        <Table />
-      </main>
-    </PlanetsProvider>
+    <nav>
+      <span>Hello, Jedi Universe!!</span>
+    </nav>
+
   );
 }
 
