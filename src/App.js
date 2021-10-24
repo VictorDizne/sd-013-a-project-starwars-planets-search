16cd729580515e5710e32555f93b370295539717
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-10-24 12:24:44 -0300

    Resolvendo requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Table from './components/Table';
+import ApiContextProvider from './context/ApiContextProvider';
+
 function App() {
   return (
-    <span>Hello, App!</span>
+    <ApiContextProvider>
+      <Table />
+    </ApiContextProvider>
   );
 }
 
