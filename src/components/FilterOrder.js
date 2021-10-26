87d2ab9424910f46f0cb26b13de47e59527633c3
FernandoLimas <nando_filipe@hotmail.com>
2021-10-26 02:48:43 -0300

    faz sort

--- src/components/FilterOrder.js
+++ src/components/FilterOrder.js
@@ -5,7 +5,7 @@ import MyContext from '../context/MyContext';
 export default function FilterOrder() {
   const { data, setData } = useContext(MyContext);
 
-  const [order, setOrder] = useState({
+  const [orderSort, setOrderSort] = useState({
     column: 'Name',
     order: 'ASC',
   });
@@ -19,10 +19,35 @@ export default function FilterOrder() {
   ];
 
   function handleChange({ target: { value } }) {
-    setOrder({
-      ...order,
+    setOrderSort({
+      ...orderSort,
       column: value,
     });
+  }
+
+  function ascOrDesc({ target: { value } }) {
+    setOrderSort({
+      ...orderSort,
+      order: value,
+    });
+  }
+
+  function clickOrder() {
+    const { order, column } = orderSort;
+    const sub = -1;
+    const filter = [...data];
+
+    // condição
+    if (order === 'ASC') {
+      filter.sort((a, b) => (a[column] > b[column] ? 1 : sub))
+        .sort((a, b) => a[column] - b[column]);
+    }
+    if (order === 'DESC') {
+      filter.sort((a, b) => (a[column] < b[column] ? 1 : sub))
+        .sort((a, b) => b[column] - a[column]);
+    }
+
+    setData(filter);
   }
 
   return (
@@ -36,7 +61,7 @@ export default function FilterOrder() {
       <input
         value="ASC"
         type="radio"
-        onChange={ asc }
+        onChange={ ascOrDesc }
         data-testid="column-sort-input-asc"
       />
 
@@ -44,13 +69,13 @@ export default function FilterOrder() {
       <input
         value="DESC"
         type="radio"
-        onChange={ DESC }
+        onChange={ ascOrDesc }
         data-testid="column-sort-input-desc"
       />
 
       <button
         type="button"
-        onClick={ b }
+        onClick={ clickOrder }
         data-testid="column-sort-button"
       >
         order
