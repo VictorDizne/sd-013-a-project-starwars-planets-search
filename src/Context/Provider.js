3bab38d81a4af93c8afa923075edbb7044a019f3
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-10-26 10:46:44 -0300

    provider

---
+++ src/Context/Provider.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Context from './Context';
+
+function AppProvider({ children }) {
+  const [planets, setPlanets] = useState();
+  const [data, setPlanetData] = useState([]);
+  const [filters, setFilter] = useState({
+    filterByName: {
+      name: '',
+    },
+    filterByNumericValues: [],
+    order: {
+      column: 'name',
+      sort: 'ASC',
+    },
+  });
+
+  return (
+    <Context.Provider
+      value={ {
+        data,
+        setPlanetData,
+        filters,
+        setFilter,
+        planets,
+        setPlanets } }
+    >
+      {children}
+    </Context.Provider>
+  );
+}
+AppProvider.propTypes = {
+  children: PropTypes.objectOf(Object).isRequired,
+};
+
+export default AppProvider;
