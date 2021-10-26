f8caf631d0d26e564136ed0fd66449f5feb29f0b
FernandoLimas <nando_filipe@hotmail.com>
2021-10-26 02:14:20 -0300

    componente filterOrder com elementos input

--- src/components/FilterOrder.js
+++ src/components/FilterOrder.js
@@ -2,9 +2,43 @@
 import React from 'react';
 
 export default function FilterOrder() {
+  const colunas = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
   return (
     <div>
       SortPlanetas
+      <select onChange={ handleChange }>
+        {colunas}
+      </select>
+
+      ASC
+      <input
+        value="ASC"
+        type="radio"
+        onChange={ asc }
+        data-testid="column-sort-input-asc"
+      />
+
+      DESC
+      <input
+        value="DESC"
+        type="radio"
+        onChange={ DESC }
+        data-testid="column-sort-input-desc"
+      />
+
+      <button
+        type="button"
+        onClick={ b }
+        data-testid="column-sort-button"
+      >
+        order
+      </button>
     </div>
   );
 }
